5e40c24ebf40061a1310eced295a171b6454a66d
Sean Shubin <sshubin@cj.com>
2023-03-27 11:34:40 -0700

    token tostring

--- example-calculator/src/main/kotlin/com/seanshubin/kotlin/compiler/example/calculator/EntryPoint.kt
+++ example-calculator/src/main/kotlin/com/seanshubin/kotlin/compiler/example/calculator/EntryPoint.kt
@@ -5,6 +5,16 @@ import com.seanshubin.kotlin.compiler.example.calculator.TokenCompiler.toTokenCu
 object EntryPoint {
     @JvmStatic
     fun main(args: Array<String>) {
+//        complexSample()
+        simpleSample()
+    }
+
+    private fun simpleSample(){
+        val input = "1 + 2 - 3"
+        val tokenCursor = input.toTokenCursor()
+        tokenCursor.reifyAll().forEach(::println)
+    }
+    private fun complexSample(){
         val input = "((1 + 2) * 3 + 8 / (3 + 1) - 1) / 5 - 1"
         val tokenCursor = input.toTokenCursor()
         tokenCursor.reifyAll().forEach(::println)
--- example-calculator/src/main/kotlin/com/seanshubin/kotlin/compiler/example/calculator/Token.kt
+++ example-calculator/src/main/kotlin/com/seanshubin/kotlin/compiler/example/calculator/Token.kt
@@ -1,12 +1,28 @@
 package com.seanshubin.kotlin.compiler.example.calculator
 
 interface Token {
-    object OpenParen : Token
-    object CloseParen : Token
-    data class WhitespaceBlock(val text: String) : Token
-    object Plus : Token
-    object Minus : Token
-    object Times : Token
-    object Divide : Token
-    data class Number(val x: Int) : Token
+    object OpenParen : Token{
+        override fun toString(): String = "OpenParen"
+    }
+    object CloseParen : Token {
+        override fun toString(): String = "CloseParen"
+    }
+    data class WhitespaceBlock(val text: String) : Token{
+        override fun toString(): String = "WhitespaceBlock('$text')"
+    }
+    object Plus : Token {
+        override fun toString(): String = "Plus"
+    }
+    object Minus : Token {
+        override fun toString(): String = "Minus"
+    }
+    object Times : Token {
+        override fun toString(): String = "Times"
+    }
+    object Divide : Token {
+        override fun toString(): String = "Divide"
+    }
+    data class Number(val x: Int) : Token {
+        override fun toString(): String = "Number($x)"
+    }
 }
